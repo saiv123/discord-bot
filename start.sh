2b17a1036812f844165a1a0230c99e8b37d3f20f
Sai <saiveeravelli2000@gmail.com>
2020-05-11 23:04:34 -0700

    moving bot to git

---
+++ start.sh
@@ -0,0 +1,2 @@
+screen -dmS discordbot sudo python3 /home/pi/discord_bot_python/bot.py
+screen -ls
---
+++ stop.sh
@@ -0,0 +1,6 @@
+screen -X -S "discordbot" quit
+
+screen -X -S "discordbot" stuff "^C"
+screen -X -S "discordbot" stuff "^C"
+screen -X -S "discordbot" stuff "^C"
+screen -X -S "discordbot" stuff "^C"
