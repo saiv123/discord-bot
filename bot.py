4f0664d390b42e54b5b51914416bfa1597580354
saiv123 <saiveeravelli2000@gmail.com>
2020-05-14 00:35:21 -0800

    something

--- bot.py
+++ bot.py
@@ -36,8 +36,7 @@ def isOwner(ctx):
     return False
 
 def msgReturn(type):
-    f= open("msg.json",'r')
-    data = json.load(f)
+    data = json.load(open("msg.json"))
     typeM = data[type]
     msgData = random.choice(typeM)
     del data, typeM, f
