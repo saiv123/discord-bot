1d703ffd1a87db1b6260e29c90fecaef9fb4df25
saiv123 <saiveeravelli2000@gmail.com>
2020-05-14 13:57:42 -0800

    ctx is a message objt

--- bot.py
+++ bot.py
@@ -177,7 +177,7 @@ async def off(ctx):
 
 @bot.command()
 async def porn(ctx):
-    if(message.guild is None and message.author != bot.user):
+    if(ctx.guild is None and message.author != bot.user):
         await ctx.send("You Dumb stupid you are not allowed to use this command in dms")
     else:
         if(ctx.channel.is_nsfw()):
