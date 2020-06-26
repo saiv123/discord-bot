fb544334d12e34c144530c655b257fb174b3380a
saiv123 <saiveeravelli2000@gmail.com>
2020-06-26 09:09:13 -0800

    Update bot.py
    help command change

--- bot.py
+++ bot.py
@@ -105,6 +105,7 @@ async def help(ctx):
     embed.add_field(name='$quote', value='Give you heart warming quotes', inline=False)
     embed.add_field(name='$nsfw', value='will give you a random nsfw image\nyou can choose a category from $nsfw category\nfrom the list you have to spell out the category excatly how it is sent to you as\n$nsfw [category]', inline=False)
     embed.add_field(name='$hi', value='Will send hi back to you', inline=False)
+    embed.add_field(name='$contactOwner', value='Will give you information on how to conact owner for support', inline=False)
 
 
     await channel.send(embed=embed)
