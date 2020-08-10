138adb6b40dea78c6875c73a9855d4aae0768010
Pop101 <leon.leibmann@gmail.com>
2020-08-09 21:05:24 -0700

    Fully utilized new quote structure

--- libraries/quotes.py
+++ libraries/quotes.py
@@ -17,10 +17,14 @@ def getQuoteApi():
         "GET", url, headers=headers, params=querystring)
 
     quote = json.loads(response.text)
-    qText = quote["content"]
-    aText = quote["originator"]["name"]
+    qDict = dict()
 
-    return {'quote':qText,'author':aText}
+    qDict['quote'] = quote["content"]
+    qDict['author'] = quote["originator"]["name"]
+    qDict['source'] = quote['url']
+    qDict['tag'] = quote['tags'][0]
+    
+    return qDict
 
 
 def getQuoteJSON():
@@ -31,3 +35,6 @@ def getQuoteJSON():
         while len(quote) < 2:  # Just make sure we don't get a blank line
             quote = random.choice(l)
     return {'quote': quote}
+
+if __name__ == "__main__":
+    getQuoteApi()
