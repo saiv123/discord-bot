1531a174685aed5b7c5c675f57aa6a6ece6d00e8
saiv123 <saiveeravelli2000@gmail.com>
2020-09-11 10:53:09 -0800

    trying to debug

--- bot.py
+++ bot.py
@@ -102,6 +102,7 @@ async def on_command_error(ctx, error):
             msgSend = "Sorry but that is not a valid command\nYou can add suggestions at https://github.com/saiv123/discord-bot/issues/new/choose"
 
     await ctx.send(msgSend)
+    traceback.print_exception()
     print(error)
     print(traceback.format_exc()) # Attempt to print exception
 
