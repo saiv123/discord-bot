018128c20d701f406b2929a1c4f93fd7ed346096
saiv123 <saiveeravelli2000@gmail.com>
2020-09-12 15:18:37 -0800

    added a new thing to bot dont question it

--- bot.py
+++ bot.py
@@ -72,7 +72,9 @@ async def on_message(message):
     #     other = await bot.fetch_user(message.author.id)
     #     with open(nameNote, 'a') as file:
     #         file.write(str(datetime.datetime.now()) + " " +other.name + " -- " + message.content + "\n")
-    if "corn" in message.content.lower() and message.author != bot.user:
+    if "456247671506599936" in message.content and message.author != bot.user:
+        await ctx.send("HEY! <@456247671506599936> YOUR MONTY FUCKING SUCKS <3~ ash aka motorcycle gal that loves ya")
+    elif "corn" in message.content.lower() and message.author != bot.user:
         channel = message.channel
         await channel.send("https://cdn.discordapp.com/attachments/654783232969277453/738997605039603772/Corn_is_the_best_crop__wheat_is_worst.mp4")
     elif "bird" in message.content.lower() and message.author != bot.user:
