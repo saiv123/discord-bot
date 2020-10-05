b7b003eb178bad07ea4f0e3aad5e1c2fd4d2e350
saiv123 <saiveeravelli2000@gmail.com>
2020-10-04 18:16:29 -0800

    moved the command to server admin commands

--- bot.py
+++ bot.py
@@ -505,6 +505,37 @@ async def ping(ctx):
 ###Server Admin Commands###
 ###########################
 
+#give information on the user
+@bot.command()
+async def userinfo(ctx, target: Optional[Member]):
+    if ctx.author.guild_permissions.administrator:
+        x = ctx.guild.members
+        if target in x:
+             roles = [role for role in member.roles[1:]]
+             embed = discord.Embed(title="User information", colour=discord.Color.gold(), timestamp=datetime.utcnow())
+
+             embed.set_author(name=target.name, icon_url=target.avatar_url)
+
+             embed.set_thumbnail(url=target.avatar_url)
+
+             embed.set_footer(text="Mr.KapiBara", icon_url="https://cdn.discordapp.com/attachments/618434755981213716/718861010223497236/kapi-1.png")
+
+             fields = [("Name", str(target), False),
+                   ("ID", target.id, False),
+                   ("Status", str(target.status).title(), False),
+                   (f"Roles ({len(roles)})", " ".join([role.mention for role in roles]), False),
+                   ("Created at", target.created_at.strftime("%d/%m/%Y %H:%M:%S"), False),
+                   ("Joined at", target.joined_at.strftime("%d/%m/%Y %H:%M:%S"), False)]
+
+             for name, value, inline in fields:
+                    embed.add_field(name=name, value=value, inline=inline)
+
+             await ctx.send(embed=embed)
+        else:
+            await ctx.send(f'You have to ping soneone from this server')
+    else:
+        await ctx.send(f'Not enough permissions')
+
 def hasAdminRole(member:discord.member, bot):
     if member.guild_permissions.administrator:
         return True
@@ -596,39 +627,6 @@ async def test(ctx):
         await ctx.send(ctx.message.author.mention)
     else:
         await ctx.send("no mention")
-        
-@bot.command()
-async def userinfo(ctx, target: Optional[Member]):
-    if ctx.author.guild_permissions.administrator:
-        x = ctx.guild.members
-        if target in x:
-             roles = [role for role in member.roles[1:]]
-             embed = discord.Embed(title="User information", colour=discord.Color.gold(), timestamp=datetime.utcnow())
-
-             embed.set_author(name=target.name, icon_url=target.avatar_url)
-
-             embed.set_thumbnail(url=target.avatar_url)
-
-             embed.set_footer(text="Mr.KapiBara", icon_url="https://cdn.discordapp.com/attachments/618434755981213716/718861010223497236/kapi-1.png")
-
-             fields = [("Name", str(target), False),
-                   ("ID", target.id, False),
-                   ("Status", str(target.status).title(), False),
-                   (f"Roles ({len(roles)})", " ".join([role.mention for role in roles]), False),
-                   ("Created at", target.created_at.strftime("%d/%m/%Y %H:%M:%S"), False),
-                   ("Joined at", target.joined_at.strftime("%d/%m/%Y %H:%M:%S"), False)]
-
-             for name, value, inline in fields:
-                    embed.add_field(name=name, value=value, inline=inline)
-
-             await ctx.send(embed=embed)
-        else:
-            await ctx.send(f'You have to ping soneone from this server')
-    else:
-        await ctx.send(f'Not enough permissions')
-python
-
-
 
 # runs the bot after all the methods have been loaded to memory
 bot.run(TOKEN)
