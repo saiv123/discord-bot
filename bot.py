199ac0163df22bf8d98f87972907d89fd300495a
saiv123 <saiveeravelli2000@gmail.com>
2020-10-17 13:23:25 -0800

    member was not defined

--- bot.py
+++ bot.py
@@ -505,6 +505,7 @@ async def ping(ctx):
 #give information on the user
 @bot.command()
 async def userinfo(ctx):
+    member = ctx.guild.get_member(ctx.author.id)
     if ctx.author.guild_permissions.administrator:
         x = ctx.guild.members
         if ctx.message.mentions[0].id != 0:
