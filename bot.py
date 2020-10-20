03de1f4a0e6f292ba164911dbc4a9c8f94e322e7
Pop101 <leon.leibmann@gmail.com>
2020-10-19 17:48:08 -0700

    Fixed length calculation

--- bot.py
+++ bot.py
@@ -320,8 +320,8 @@ async def song(ctx, *, songName:str):
 SPACE_LEN_HARD_CAP = 4000
 @bot.command()
 async def space(ctx, *, space:int=1, msg:str):
-    if not isOwner(ctx) and len(msg)*space <= SPACE_LEN_HARD_CAP:
-        await ctx.send('That message would be {0} characters, waaaay higher than the limit of {1}. Chill.'.format(len(msg)*space, SPACE_LEN_HARD_CAP))
+    if not isOwner(ctx) and len(msg)*(space+1) <= SPACE_LEN_HARD_CAP:
+        await ctx.send('That message would be {0} characters, waaaay higher than the limit of {1}. Chill.'.format(len(msg)*(space+1), SPACE_LEN_HARD_CAP))
         return
     
     to_send = (' '*max(1, space)).join(msg.replace(' ',''))
