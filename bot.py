5d369339fd1854701689a8dc798d041bcd532c46
Pop101 <leon.leibmann@gmail.com>
2020-12-21 19:23:21 -0800

    remembered forgotton colon

--- bot.py
+++ bot.py
@@ -477,7 +477,7 @@ async def rpsc(ctx, user:discord.User, *, level=1):
     if level > 1:
         msg = msg+'-'+str(level*2+1)
     
-    for embed in add_to_embed(f'Your challenge to {user.name}',msg+'\nCheck your DMs!')
+    for embed in add_to_embed(f'Your challenge to {user.name}',msg+'\nCheck your DMs!'):
         embed.set_footer(text='Challenge sent by: ' + ctx.message.author.name, icon_url=ctx.message.author.avatar_url)
         await ctx.send(embed=embed)
 
