7d34154812b10db8bd968c56e29e385d735d7f78
Pop101 <leon.leibmann@gmail.com>
2020-12-21 19:10:49 -0800

    FIXXXXXXXXXXXXXXXXX

--- bot.py
+++ bot.py
@@ -499,7 +499,6 @@ async def rpsc(ctx, user:discord.User, *, level=1):
             
             response = msg.content.lower().replace(' ','_').replace('\n','')
             choice = getClosestFromList(['abort','rules']+symbol_names,response.lower())
-            print('sus1')
             if distance(response, choice) >= len(response)*0.3:
                 await _user.send(embed=add_to_embed(choice, 'No option recognized, try again'))
 
@@ -512,24 +511,17 @@ async def rpsc(ctx, user:discord.User, *, level=1):
                 i -= 1
             else: # If neither rules or abort, it is correct
                 break
-        print('sus2')
         return choice
 
     
     # Get your response
     your_choice = await get_response(ctx.message.author, title=f'Your challenge to {user.name}')
-    print(symbol_names)
-    print(your_choice)
-    print('sus3')
     if your_choice == -1:
-        print('sus4')
         await ctx.message.author.send(embed=add_to_embed(f'Your challenge to {user.name}', 'Challenge cancelled!')[0])
-        print('sus5')
         await ctx.send(embed=add_to_embed(f'{ctx.message.author.name}\'s challenge', 'Challenge cancelled!')[0])
         return
     your_choice = symbol_names.index(your_choice)
-    print('sus6')
-    await ctx.message.author.send(embed=add_to_embed(f'Your challenge to {user.name}',f'You chose {symbol_names[your_choice]}'))
+    await ctx.message.author.send(embed=add_to_embed(f'Your challenge to {user.name}',f'You chose {symbol_names[your_choice]}')[0])
 
     # Get other person's response
     await user.send(embed=add_to_embed(None, f'{ctx.message.author.name} has challenged you to rock-paper-scissors-'+str(level*2+1) if level > 1 else '')[0])
