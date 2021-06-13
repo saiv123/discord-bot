dab13929bebf5178aa106d10512ee89fb1fcc91f
saiv123 <saiveeravelli2000@gmail.com>
2021-06-12 19:14:05 -0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -35,7 +35,7 @@ client = wolframalpha.Client(id)
 # Load all cogs
 
 bot.load_extension("slash_commands.admin")
-# bot.load_extension("slash_commands.fun")
+bot.load_extension("slash_commands.fun")
 # bot.load_extension("slash_commands.info")
 # bot.load_extension("slash_commands.math")
 # bot.load_extension("slash_commands.memes")
