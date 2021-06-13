96597c38fe3670f2b51feeab85ea5960c21f963b
saiv123 <saiveeravelli2000@gmail.com>
2021-06-12 19:26:37 -0800

    Update info.py

--- slash_commands/info.py
+++ slash_commands/info.py
@@ -49,7 +49,7 @@ class info_commands(commands.Cog):
         embed = discord.Embed(colour=discord.Colour.green())
         embed.set_author(name='Invite the Bot to another server')
         embed.set_thumbnail(url='https://cdn.discordapp.com/avatars/314578387031162882/e4b98a4a9ca3315ca699ffe5cba5b8f1.png?size=1024')
-        embed.add_field(name='Please invite me to other Discords',value='[Invite bot to server](https://discord.com/api/oauth2/authorize?client_id=314578387031162882&permissions=8&scope=bot)', inline=False)
+        embed.add_field(name='Please invite me to other Discords',value='[Invite bot to server](https://discord.com/api/oauth2/authorize?client_id=314578387031162882&permissions=8&scope=bot%20applications.commands)', inline=False)
         embed.set_footer(text='Invite Requested by: ' + ctx.author.name, icon_url=ctx.author.avatar_url)
         await ctx.send(embed=embed)
     
