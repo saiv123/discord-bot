4efc0f2b7d9e9e65c0dd06e3ef506f89a468189a
Pop101 <leon.leibmann@gmail.com>
2021-06-20 01:09:38 -0700

    Basic Database Infrastructure

--- libraries/botDB.py
+++ libraries/botDB.py
@@ -1,7 +1,21 @@
-import re
-import ast
-import requests
-from bs4 import BeautifulSoup
 import sqlite3
 
-conn = sqlite3.connect('botDB.db')
+class BotDB():
+    def __init__(self, db_name='botDB'):
+        self.db_name = db_name[:db_name.find('.')] if '.' in db_name else db_name
+    def __enter__(self):
+        self.connection = sqlite3.connect(f'{self.db_name}.db')
+        self.cursor = self.connection.cursor()
+        return self
+    def __exit__(self, exc_type, exc_value, exc_traceback):
+        self.cursor.close()
+        self.connection.close()
+        del self.cursor, self.connection
+
+if __name__ == '__main__':
+    # Use the context manager to list all tables
+    with BotDB() as db:
+        db.cursor.execute('''SELECT name FROM sqlite_master WHERE type IN ('table','view') AND name NOT LIKE 'sqlite_%' ''')
+        print('Tables:\n'+'\n'.join(db.cursor.fetchall()))
+    # Very convenient
+    
