d07b10b2dd4758887410edf363e9584efafe86c2
saiv123 <saiveeravelli2000@gmail.com>
2021-06-21 10:14:03 -0800

    Update botDB.py

--- libraries/botDB.py
+++ libraries/botDB.py
@@ -7,16 +7,16 @@ def move_json():
     with BotDB(db_name="msg.db") as db:
         data = json.load(open("..\msg.json"))
         for key in data: 
-            db.execute(f'CREATE TABLE IF NOT EXISTS (?) (list TEXT PRIMARY KEY NOT NULL)',(key,))
+            db.execute(f'CREATE TABLE IF NOT EXISTS {key} (list TEXT PRIMARY KEY NOT NULL)')
             typeM = data[key]
-            db.execute(f'DELETE FROM (?)',(key,)) #deletes all rows from table
+            db.execute(f'DELETE FROM {key}') #deletes all rows from table
             for msg in typeM:
-                db.execute(f'INSERT INTO (?) VALUES (?)',(key,str(msg),))
+                db.execute(f'INSERT INTO {key} VALUES (?)',(str(msg),))
 
 def add_to_table(key: str, DBname: str, value: str):
     with botDB(db_name=DBname) as db:
-        db.execute(f'CREATE TABLE IF NOT EXISTS (?) (value TEXT PRIMARY KEY NOT NULL)',(key,))
-        db.execute(f'INSERT INTO (?) VALUES (?)',(key, value,))
+        db.execute(f'CREATE TABLE IF NOT EXISTS {key} (value TEXT PRIMARY KEY NOT NULL)')
+        db.execute(f'INSERT INTO {key} VALUES (?)',(value,))
 
 def del_table_rows(key: str, DBname: str):
     with botDB(db_name=DBname) as db:
