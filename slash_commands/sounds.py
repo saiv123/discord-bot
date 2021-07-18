f7f7e1a7152ee4ef3c8a83451829b9c5b874ce31
saiv123 <saiveeravelli2000@gmail.com>
2021-07-18 11:59:24 -0800

    Update sounds.py

--- slash_commands/sounds.py
+++ slash_commands/sounds.py
@@ -21,7 +21,7 @@ class sound_commands(commands.Cog):
     
     @cog_ext.cog_slash(name='aqua', options=aqua_options, description='Makes baby noises', guild_ids=[601247340887670792, 648012188685959169])
     async def aqua(self, ctx: SlashContext, sound: String):
-        path = './sounds/'+String+'.mp3'
+        path = './sounds/'+sound+'.mp3'
         try:
             trust = [288861358555136000, 240636443829993473, 361275648033030144]
             if ctx.author.id in trust:
