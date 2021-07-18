63d72df87ddcc135c723a0bc5510f1e23c96e1f4
saiv123 <saiveeravelli2000@gmail.com>
2021-07-18 10:12:10 -0800

    Update sounds.py

--- slash_commands/sounds.py
+++ slash_commands/sounds.py
@@ -13,12 +13,11 @@ class sound_commands(commands.Cog):
     
     @cog_ext.cog_slash(name='aqua', description='Makes you baby noises', guild_ids=[601247340887670792, 648012188685959169])
     async def aqua(self, ctx: SlashContext):
-        if(ctx.author.voice){
-            channel = ctx.message.author.voice.channel
+        try:
+            channel = ctx.author.voice.channel
             voice = await channel.connect()
             source = FFmpegPCMAudio('./sounds/good_child.mp3')
             player = voice.play(source)
             await ctx.send("DONE", hidden=True)
-        }else{
+        except AttributeError as e:
             await ctx.send("OOF you cant run this command or your not in vc ;(", hidden=True)
-        }
