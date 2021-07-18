75b8646a21189aabd689d28a5ac6957b008fc181
saiv123 <saiveeravelli2000@gmail.com>
2021-07-18 15:41:06 -0800

    Update sounds.py

--- slash_commands/sounds.py
+++ slash_commands/sounds.py
@@ -4,6 +4,8 @@ from discord_slash import cog_ext, SlashContext
 from discord_slash.utils.manage_commands import create_option
 from discord import FFmpegPCMAudio
 
+import os
+
 def setup(bot):
     bot.add_cog(sound_commands(bot))
 
@@ -11,7 +13,7 @@ class sound_commands(commands.Cog):
     aqua_options = [
         {
             "name": "sound",
-            "description": "what sound you want to use. if you dont know the file name use /list",
+            "description": "what sound you want to use. If you dont know the file name use /listsound",
             "type": 3,
             "required": True
         }
@@ -19,6 +21,9 @@ class sound_commands(commands.Cog):
     def __init__(self, bot):
         self.bot = bot 
     
+    class NotTrusted(Exception):
+        pass
+
     @cog_ext.cog_slash(name='aqua', options=aqua_options, description='Makes baby noises', guild_ids=[601247340887670792, 648012188685959169])
     async def aqua(self, ctx: SlashContext, sound: str):
         path = './sounds/'+sound+'.mp3'
@@ -34,7 +39,23 @@ class sound_commands(commands.Cog):
                 await ctx.guild.voice_client.disconnect()
             else:
                 await ctx.send("i solemnly swear i am up to no good")
-                raise AttributeError('Dont worry about it')
+                raise NotTrusted('Dont worry about it')
         except AttributeError as e:
             print(e)
-            await ctx.send("OOF you cant run this command or your not in vc ;(", hidden=True)
+            await ctx.send("your not in vc ;(", hidden=True)
+        except NotTrusted as e:
+            print(e)
+            await ctx.send("OOF you dont have permitions to run this command.", hidden=True)
+    
+    @cog_ext.cog_slash(name='listsound', description='List of sounds', guild_ids=[601247340887670792, 648012188685959169])
+    async def listsound(self, ctx: SlashContext):
+        trust = [288861358555136000, 240636443829993473, 361275648033030144]
+        if ctx.author.id not in trust:
+            await ctx.send("OOF you dont have permitions to run this command.", hidden=True)
+            return
+        path = './sounds'
+        files = os.listdir(path)
+        temp = ''
+        for f in files:
+            temp += f+"\n"
+        await ctx.send(temp, hidden=True)
