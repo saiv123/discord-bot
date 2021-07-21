4711b62da11e1c4717c5654f2c043ea60e46cff9
saiv123 <saiveeravelli2000@gmail.com>
2021-07-21 10:25:28 -0800

    Update sounds.py

--- slash_commands/sounds.py
+++ slash_commands/sounds.py
@@ -13,7 +13,7 @@ class sound_commands(commands.Cog):
     aqua_options = [
         {
             "name": "sound",
-            "description": "what sound you want to use. If you dont know the file name use /listsound",
+            "description": "what sound you want to use. If you dont know the file name use /aquasound",
             "type": 3,
             "required": True
         }
@@ -26,7 +26,7 @@ class sound_commands(commands.Cog):
 
     @cog_ext.cog_slash(name='aqua', options=aqua_options, description='Makes baby noises', guild_ids=[601247340887670792, 648012188685959169])
     async def aqua(self, ctx: SlashContext, sound: str):
-        path = './sounds/'+sound+'.mp3'
+        path = './sounds/aqua/'+sound+'.mp3'
         try:
             trust = [288861358555136000, 240636443829993473, 361275648033030144]
             if ctx.author.id in trust:
@@ -47,13 +47,13 @@ class sound_commands(commands.Cog):
             print(e)
             await ctx.send("OOF you dont have permitions to run this command.", hidden=True)
     
-    @cog_ext.cog_slash(name='listsound', description='List of sounds', guild_ids=[601247340887670792, 648012188685959169])
-    async def listsound(self, ctx: SlashContext):
+    @cog_ext.cog_slash(name='aquasound', description='List of sounds', guild_ids=[601247340887670792, 648012188685959169])
+    async def aquasound(self, ctx: SlashContext):
         trust = [288861358555136000, 240636443829993473, 361275648033030144]
         if ctx.author.id not in trust:
             await ctx.send("OOF you dont have permitions to run this command.", hidden=True)
             return
-        path = './sounds'
+        path = './sounds/aqua'
         files = os.listdir(path)
         temp = ''
         for f in files:
