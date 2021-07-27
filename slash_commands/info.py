e26f0aa3516e6e157c82e7ae1613de2043c376f2
saiv123 <saiveeravelli2000@gmail.com>
2021-07-27 11:14:30 -0800

    adding cpu stats

--- slash_commands/info.py
+++ slash_commands/info.py
@@ -61,13 +61,17 @@ class info_commands(commands.Cog):
     @cog_ext.cog_slash(name='stats', description='What am I up to?')
     @has_cooldown(10)
     async def stats(self, ctx: SlashContext):
-        quote = quotes.getQuoteApi()
+        #get the memory stats of the computer
         raw = subprocess.Popen(['free', '-h'], stdout = subprocess.PIPE)
         output = str(raw.communicate()).split('\\n')
         temp = output[1].split(" ")
         while '' in temp: temp.remove('')
         percent = convertInt(temp[2]) / convertInt(temp[1])
         memory = temp[2]+"/"+temp[1]+"-"+percent+"%"
+
+        #get the cpu stats of the computer
+        cpu_percent = getCPUStats()
+        CPU = str(cpu_percent)+"%"
         # calculating time bot has been on
         tso = time.time()
         msg = time.strftime("%H Hours %M Minutes %S Seconds",time.gmtime(tso - ts))
@@ -77,7 +81,7 @@ class info_commands(commands.Cog):
         embed.set_thumbnail(url="https://hotemoji.com/images/dl/h/ten-o-clock-emoji-by-twitter.png")
         embed.add_field(name='I have been awake for:', value=msg, inline=True)
         embed.add_field(name='Memory:',value=memory,inline=True)
-        embed.add_field(name='CPU:',value="N/A",inline=True)
+        embed.add_field(name='CPU:',value=CPU,inline=True)
         # embed.add_field(name='My core body temperature:',value=temp.replace("temp=", ""), inline=True)
         # embed.add_field(name="Quote cus I know you're bored:", value='"' +quote['quote'] + '"\n\t~' + quote['author'], inline=False)
 
@@ -105,4 +109,10 @@ class info_commands(commands.Cog):
         string = string.replace('G','*1073741824').replace('M','*1048576').replace('K','*1024')
         num = float(eval(string))
         return num
-
+    
+    def getCPUStats():
+        raw = subprocess.Popen(['mpstat', '-u', '-o', 'JSON'], stdout = subprocess.PIPE)
+        data = json.loads(raw.communicate()[0])
+        user = data["sysstat"]["hosts"][0]["statistics"][0]["cpu-load"][0]["usr"]
+        system = data["sysstat"]["hosts"][0]["statistics"][0]["cpu-load"][0]["sys"]
+        return user+system
