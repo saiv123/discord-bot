6e87addb31bc8a53020ec24c8f63a79350140a43
saiv123 <saiveeravelli2000@gmail.com>
2021-08-08 08:33:36 -0800

    Update sounds.py

--- slash_commands/sounds.py
+++ slash_commands/sounds.py
@@ -16,7 +16,7 @@ async def play(ctx, path):
     voice = await channel.connect()
     source = FFmpegPCMAudio(path)
     player = voice.play(source)
-    time = files.getTime(path)
+    time = getTime(path)
     await ctx.send("DONE", hidden=True)
     await asyncio.sleep(time)
     await ctx.guild.voice_client.disconnect()
@@ -27,22 +27,21 @@ class NotTrusted(Exception):
 def setup(bot):
     bot.add_cog(sound_commands(bot))
 
-class files():
-    def get(path:str):
-    files = os.listdir(path)
-    temp = ''
-    for f in files:
-        i = f.index('.')
-        f = f[:i]
-        temp += f+"\n"
-    return temp
+def getFile(path:str):
+files = os.listdir(path)
+temp = ''
+for f in files:
+    i = f.index('.')
+    f = f[:i]
+    temp += f+"\n"
+return temp
 
-    def getTime(path:str):
-        raw = subprocess.Popen(['soxi', '-D', path], stdout = subprocess.PIPE)
-        output = str(raw.communicate()[0]).split('\\n')
-        rawTime = output[0].split('\'')
-        rawTime = float(math.ceil(float(rawTime[1])))
-        return rawTime+1.0
+def getTime(path:str):
+    raw = subprocess.Popen(['soxi', '-D', path], stdout = subprocess.PIPE)
+    output = str(raw.communicate()[0]).split('\\n')
+    rawTime = output[0].split('\'')
+    rawTime = float(math.ceil(float(rawTime[1])))
+    return rawTime+1.0
 
 class sound_commands(commands.Cog):
     aqua_options = [
@@ -88,7 +87,7 @@ class sound_commands(commands.Cog):
             await ctx.send("OOF you dont have permitions to run this command.", hidden=True)
             return
         path = './sounds/aqua'
-        await ctx.send(files.get(path), hidden=True)
+        await ctx.send(getFile(path), hidden=True)
 
     @cog_ext.cog_slash(name='alex', options=alex_options, description='Makes sounds', guild_ids=[531614305733574666, 648012188685959169])
     async def alex(self, ctx: SlashContext, sound: str):
@@ -112,4 +111,4 @@ class sound_commands(commands.Cog):
             await ctx.send("OOF you dont have permitions to run this command.", hidden=True)
             return
         path = './sounds/alex'
-        await ctx.send(files.get(path), hidden=True)
+        await ctx.send(getFile(path), hidden=True)
