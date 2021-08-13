ff0e01118049c6ec0f50b13e4ed7a46af1b2f3f7
saiv123 <saiveeravelli2000@gmail.com>
2021-08-13 11:15:12 -0800

    Update dev.py

--- dev.py
+++ dev.py
@@ -13,11 +13,18 @@ def setup(bot):
 class dev(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-    
+    reload_op = [
+        {
+            "name": "update",
+            "description": "reloads all the cogs",
+            "type": 3,
+            "required": False
+        }
+    ]
     # command will change offten to test out commands
-    @cog_ext.cog_slash(name='update', description='reloads all the cogs' )
+    @cog_ext.cog_slash(name='update', options=reload_op, description='reloads all the cogs' )
     async def reload(self, ctx, *, cog: str="all"):
-        if not isOwner(ctx):
+        if not isOwner(ctx): return
         if cog == "all":
             cogs = []
             for cog in self.bot.cogs:
