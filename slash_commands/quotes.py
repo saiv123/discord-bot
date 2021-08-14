ae114f58211b09e897eeb8d9286db92098754295
saiv123 <saiveeravelli2000@gmail.com>
2021-08-14 11:00:05 -0800

    small update to quotes

--- slash_commands/quotes.py
+++ slash_commands/quotes.py
@@ -19,21 +19,21 @@ class quotes(commands.Cog):
     def __init__(self, bot):
         self.bot = bot 
     
-    @cog_ext.cog_slash(name='quote', description='Sends a heartwarming quote')
+    @cog_ext.cog_subcommand(base="quote", name='HeartWarming', description='Sends a heartwarming quote')
     async def quote(self, ctx: SlashContext):
         quote = apis.quote_to_discord_embed(quotes.getQuoteJSON())
         quote.set_thumbnail(url='https://clipart.info/images/ccovers/1531011033heart-emoji.png')
         quote.set_footer(text='Quote Requested by: ' + ctx.author.name, icon_url=ctx.author.avatar_url)
         await ctx.send(embed=quote)
 
-    @cog_ext.cog_slash(name='randquote', description='Get a unique quote')
+    @cog_ext.cog_subcommand(base="quote", name='randquote', description='Get a unique quote')
     async def randquote(self, ctx: SlashContext):
         quote = quotes.getQuoteApi()
         embed = apis.quote_to_discord_embed(quote)
         embed.set_footer(text='Quote Requested by: ' + ctx.author.name, icon_url=ctx.author.avatar_url)
         await ctx.send(embed=embed)
     
-    @cog_ext.cog_slash(name='advice', description='Sends a random piece of advice')
+    @cog_ext.cog_subcommand(base="quote", name='advice', description='Sends a random piece of advice')
     async def advice(self, ctx: SlashContext):
         advice = apis.advice()
         embed = apis.quote_to_discord_embed(advice)
