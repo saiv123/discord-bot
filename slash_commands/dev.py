d81302aea68249dd119ae376a0e021b9f126f642
saiv123 <saiveeravelli2000@gmail.com>
2021-08-13 19:15:13 -0800

    adding cool stuf to dev

--- slash_commands/dev.py
+++ slash_commands/dev.py
@@ -25,11 +25,13 @@ class dev(commands.Cog):
             "required": False
         }
     ]
-    
+
     # updates the scripts
     @cog_ext.cog_slash(name='update', options=reload_op, description='reloads all the cogs' )
     async def reload(self, ctx, cogType: str="all"):
+        trust = [648012188685959169, 272155212347736065]
         if not isOwner(ctx): return
+        isHidden = (ctx.guild.id in trust)
         embed = discord.Embed(title="Updating the bot...", colour=discord.Color.gold(), timestamp= datetime.fromtimestamp(time.time()))
         embed.set_footer(text='bot updated by: ' + ctx.author.name, icon_url=ctx.author.avatar_url)
 
@@ -49,11 +51,11 @@ class dev(commands.Cog):
                 self.bot.reload_extension("slash_commands."+cog)
 
             embed.add_field(name="Update Cogs", value="Done :white_check_mark:", inline=True)
-            await ctx.send(embed=embed, hidden=True)
+            await ctx.send(embed=embed, hidden=isHidden)
         except discord.ext.commands.ExtensionNotLoaded as e:
             embed.add_field(name="Error", value=e, inline=True)
-            await ctx.send(embed=embed, hidden=True)
+            await ctx.send(embed=embed, hidden=isHidden)
             print(e)
         except:
             embed.add_field(name="Error", value=sys.exc_info()[0], inline=True)
-            await ctx.send(embed=embed, hidden=True)
+            await ctx.send(embed=embed, hidden=isHidden)
