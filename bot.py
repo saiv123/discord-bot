6f785cd031255706a2dad024c61dfd4493e96cef
saiv123 <saiveeravelli2000@gmail.com>
2021-12-07 09:02:25 -0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -102,7 +102,7 @@ async def on_message(message):
 
     if tempDate == 7 and tempMonth == 12:
         age = tempD.year - 2000
-        await user.send("Happy Birthday you are " + str(age), hidden=True)
+        await user.send("Happy Birthday you are " + str(age))
 
     # Respond to last command
     await bot.process_commands(message)
