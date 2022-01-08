fb6e22d4e78c9453a16e8ac8cd271a25511e579c
saiv123 <saiveeravelli2000@gmail.com>
2022-01-07 19:03:24 -0800

    Update jail.py

--- slash_commands/jail.py
+++ slash_commands/jail.py
@@ -109,7 +109,7 @@ class jail(commands.Cog):
             await ctx.send("You do not have permitions to use this command", hidden=False)
 
     @cog_ext.cog_slash(name="jailvc", options=vc_options, description="Set the jail vc")
-    async def jailvc(self, ctx: SlashContext, vc: discord.Channel = None):
+    async def jailvc(self, ctx: SlashContext, vc: discord.VoiceChannel = None):
         if ctx.author.id == ctx.guild.owner_id or isOwner(ctx):
             vcID = vc.id
             channels.set(ctx.guild.id, 'vc_id', vcID)
