b635166da9059ec199a06c7b70883bfa0eb2aee0
saiv123 <saiveeravelli2000@gmail.com>
2022-04-18 14:21:09 -0800

    added more scales

---
+++ scales/snek_fun.py
@@ -0,0 +1,149 @@
+import dis_snek
+
+
+import dis_snek as dis
+
+
+from libraries.helperFunctions import isOwner, splitLongStrings, add_to_embed
+from libraries.helperFunctions import checkAuthSerers
+from libraries.helperFunctions import RgbToHex, HexToRgb
+from libraries.helperFunctions import msgReturn
+import libraries.quotesLib as quotes
+
+import random
+import libraries.bonusapis as apis
+import libraries.imgutils as imgutils
+
+class Fun(dis.Scale):
+    def __init__(self, bot: dis.Snake):
+        self.bot: dis.Snake = bot
+    
+    SHOULDI_PHRASES = [
+        "Yes! Go $",
+        "No, it won't work.",
+        "Hmmm, $ might be a fine idea",
+        'Unclear, consider rewording "/"',
+        "I don't know, ask someone else about $",
+    ]
+
+    @dis.slash_command(name="space", description="Spread a message")
+    @dis.slash_option(
+        "message",
+        "What's gotta be spread",
+        dis.OptionTypes.STRING,
+        required=True,)
+    @dis.slash_option(
+        "amount",
+        "What's gotta be spread",
+        dis.OptionTypes.INTEGER,
+        required=False,)
+    async def space(self, ctx: dis.InteractionContext, message: str = "", amount: int = 1):
+        SPACE_LEN_HARD_CAP = 4000
+        exp_len = (len(message) - 1) * amount + len(message)
+        if not isOwner(ctx) and exp_len >= SPACE_LEN_HARD_CAP:
+            await ctx.send(
+                "That message would be {0} characters, waaaay higher than the limit of {1}. Chill.".format(
+                    exp_len, SPACE_LEN_HARD_CAP
+                )
+            )
+            return
+        to_send = (" " * max(1, amount)).join(message)
+        for message in splitLongStrings(to_send):
+            await ctx.send(message)
+    
+    @dis.slash_command(name="color", description="Get info on a color")
+    @dis.slash_option(
+        "input",
+        "A color as hex, rgb, or cmyk",
+        dis.OptionTypes.STRING,
+        required=True,)
+    @dis.cooldown(dis.Buckets.USER, 3, 60)
+    async def color(self, ctx: dis.InteractionContext, input: str = ""):
+        try:
+            color_dict = apis.getColor(input)
+            embed = apis.colorDictToEmbed(color_dict)
+            embed.set_author(name="[See It Here]", url=color_dict["url"])
+            embed.set_footer(
+                text="Color picked by: " + ctx.author.display_name,
+                icon_url=ctx.author.avatar.url,
+            )
+
+            await ctx.send(embed=embed)
+        except ValueError:
+            embed = dis.Embed(
+                title="Error in your input",
+                description="The given color is incorrect. Enter it in Hex, RGB, or CMYK form",
+                colour=0xFF0000,
+            )
+            embed.set_footer(
+                text="Color picked by: " + ctx.author.display_name,
+                icon_url=ctx.author.avatar.url,
+            )
+            await ctx.send(embed=embed)
+    
+    @dis.slash_command(name="shouldi", description="Should I?")
+    @dis.slash_option(
+        "question",
+        "What's the question?",
+        dis.OptionTypes.STRING,
+        required=True,)
+    async def shouldi(self, ctx: dis.InteractionContext, question: str = ""):
+        msg = " " + msg + " "
+        # msg = ' '.join(msg)
+        phrases = [
+            "Yes! Go /",
+            "No, it won't work.",
+            "Hmmm, / might be a fine idea",
+            'Unclear, consider rewording "/"',
+            "I don't know, ask someone else about /",
+        ]
+        embed = dis.Embed(
+            title="Should I...",
+            description="{}\n{}".format(msg, random.choice(phrases).replace("/", msg)),
+        )
+        embed.set_footer(text="Asked by: " + ctx.author.display_name, icon_url=ctx.author.avatar.url)
+        await ctx.send(embed=embed)
+    
+    @dis.slash_command(name="boop", description="Boop")
+    @dis.slash_option(
+        "user",
+        "Who's boopin'",
+        dis.OptionTypes.USER,
+        True,)
+    async def boop(self, ctx: dis.InteractionContext, user: dis.User = None):
+        await ctx.send(ctx.author.mention + " has Booped " + user.mention)
+    
+    @dis.slash_command(name="hugh", description="Hugs")
+    @dis.slash_option(
+        "user",
+        "Who's hugging",
+        dis.OptionTypes.USER,
+        True,)
+    async def hug(self, ctx: dis.InteractionContext, user: dis.User = None):
+        if ctx.author.id == user.id:
+            await ctx.send(self.bot.user.mention + msgReturn("hug") + user.mention + "!! :hugging:")
+        else:
+            await ctx.send(ctx.author.mention + msgReturn("hug") + user.mention + "!! :hugging:")
+    
+    @dis.slash_command(name="Hugvc", description="Hug everyone in the vc")
+    async def hugvc(self, ctx: dis.InteractionContext):
+         if ctx.author.voice is None:
+            await ctx.send("Sorry but you are not in a voice channel")
+         else:
+            noBot = []
+            people = ctx.author.voice.channel.members
+            for person in people:
+                if not person.bot:
+                    noBot.append(person)
+
+            if len(people) == 1:
+                await ctx.send("Seems your the only one, here have a hug.\n" +self.bot.user.mention + msgReturn("hug") + ctx.author.mention + "!! :hugging:")
+            elif len(people) > 1:
+                pings = ""
+                for pepes in noBot:
+                    if pepes.id != ctx.author.id:
+                        pings += pepes.user.mention
+                
+                await ctx.send(ctx.author.mention + msgReturn("hug") + pings + "!! :hugging:")
+def setup(bot):
+    Fun(bot)
---
+++ scales/snek_info.py
@@ -0,0 +1,163 @@
+import dis_snek as dis
+
+import libraries.imgutils as imgutils
+from libraries.prawn import getClosestFromList
+
+import json
+import time
+import holidays
+from datetime import date, datetime
+from dateutil.relativedelta import *
+from dateutil.easter import *
+from dateutil.rrule import *
+from dateutil.parser import *
+
+import subprocess
+import json
+
+from secret import cont
+from snek_bot import ts
+
+class Info(dis.Scale):
+    def __init__(self, bot: dis.Snake):
+        self.bot: dis.Snake = bot
+    
+    @dis.slash_command(name="help", description="Get help on a command")
+    async def help(self, ctx: dis.InteractionContext):
+        # seting up an embed
+        embed = dis.Embed(
+            description="Info on the bot and how to use it",
+            colour=dis.Colour.g,
+        )
+
+        embed.set_author(name="Help")
+        embed.set_thumbnail(
+            url="https://cdn.discordapp.com/avatars/314578387031162882/e4b98a4a9ca3315ca699ffe5cba5b8f1.png?size=1024"
+        )
+        embed.add_field(
+            name="Commands will be found on the website.",
+            value="[Link to website](https://saiv123.github.io/discord-bot/website/)",
+            inline=False,
+        )
+        embed.add_field(
+            name="Please invite me to other Discords",
+            value="[Invite bot to server](https://discord.com/api/oauth2/authorize?client_id=314578387031162882&permissions=8&scope=bot%20applications.commands)",
+            inline=False,
+        )
+
+        embed.set_footer(
+            text="Help Requested by: " + ctx.author.display_name, icon_url=ctx.author.avatar.url
+        )
+        await ctx.send(embed=embed)
+
+    @dis.slash_command(name="hi", description="Say hi to the bot")
+    async def hi(self, ctx: dis.InteractionContext):
+        embed = dis.Embed(
+            title="Hello",
+            description="Hello {0}!!!".format(ctx.author.mention),
+            colour=imgutils.randomSaturatedColor(),
+        )
+        embed.set_footer(
+            text="Sanity check by: " + ctx.author.display_name, icon_url=ctx.author.avatar.url
+        )
+        await ctx.send(embed=embed)
+    
+    @dis.slash_command(name="github", description="Get the link to the github repo")
+    async def github(self, ctx: dis.InteractionContext):
+        embed = dis.Embed(
+            title="GitHub Website for Bot",
+            description="This is where you can see how the bot works",
+            url="https://github.com/saiv123/discord-bot",
+        )
+        embed.set_footer(
+            text="Github Requested by: " + ctx.author.display_name,
+            icon_url=ctx.author.avatar.url,
+        )
+        await ctx.send(embed=embed)
+    
+    @dis.slash_command(name="invite", description="Get the link to invite the bot to your server")
+    async def invite(self, ctx: dis.InteractionContext):
+        embed = dis.Embed(colour=dis.Color.g)
+        embed.set_author(name="Invite the Bot to another server")
+        embed.set_thumbnail(
+            url="https://cdn.discordapp.com/avatars/314578387031162882/e4b98a4a9ca3315ca699ffe5cba5b8f1.png?size=1024"
+        )
+        embed.add_field(
+            name="Please invite me to other Discords",
+            value="[Invite bot to server](https://discord.com/api/oauth2/authorize?client_id=314578387031162882&permissions=8&scope=bot%20applications.commands)",
+            inline=False,
+        )
+        embed.set_footer(
+            text="Invite Requested by: " + ctx.author.display_name,
+            icon_url=ctx.author.avatar.url,
+        )
+        await ctx.send(embed=embed)
+    
+    @dis.slash_command(name="stats", description="Get the bot's stats")
+    async def stats(self, ctx: dis.InteractionContext):
+        # Get the memory stats
+        raw = subprocess.Popen(["free", "-h"], stdout=subprocess.PIPE)
+        output = str(raw.communicate()).split("\\n")
+        temp = [x for x in output[1].split(" ") if x != ""]
+        percent = memstrToNum(temp[2]) / memstrToNum(temp[1])
+        percent = "{:.2f}".format(percent * 100.0)
+        memory = f"{temp[2]}/{temp[1]} - {percent}%"
+
+        # Get CPU stats
+        cpu_percent = f"{getCPUStats()}%"
+
+        # calculating time bot has been on
+        tso = time.time()
+        msg = time.strftime("%H Hours %M Minutes %S Seconds", time.gmtime(tso - ts))
+
+        # Set up and send embed
+        embed = dis.Embed(title="Status", colour=imgutils.randomSaturatedColor())
+        embed.set_thumbnail(
+            url="https://hotemoji.com/images/dl/h/ten-o-clock-emoji-by-twitter.png"
+        )  # set the clock image
+        embed.add_field(name="I have been awake for:", value=msg, inline=False)
+        embed.add_field(name="Memory:", value=memory, inline=True)
+        embed.add_field(name="CPU:", value=cpu_percent, inline=True)
+        embed.set_footer(
+            text="Status Requested by: " + ctx.author.display_name,
+            icon_url=ctx.author.avatar.url,
+        )
+        await ctx.send(embed=embed)
+    
+    @dis.slash_command(name="contact", description="Contact my father")
+    async def contact(self, ctx: dis.InteractionContext):
+        msg = "Discord: Sai#3400\nDiscord server: <http://discord.gg/dKWV3hS>\n"
+        if (ctx.channel.id == 674120261691506688):  # channel specific to my discord server
+            msg += cont
+
+        embed = dis.Embed(title="Contact", colour=imgutils.randomSaturatedColor())
+        embed.add_field(name="♥ Take Care", value=msg, inline=True)
+
+        await ctx.send(embed=embed, hidden=True)
+    
+    @dis.slash_command(name="ping", description="Ping the bot")
+    async def ping(self, ctx: dis.InteractionContext):
+        embed = dis.Embed(title="Pong!", colour=imgutils.randomSaturatedColor())
+        embed.add_field(name="Latency:", value=f"{ctx.bot.latency * 1000:.2f}ms", inline=True)
+        embed.set_footer(
+            text="Ping Measured by: " + ctx.author.display_name, icon_url=ctx.author.avatar.url
+        )
+        await ctx.send(embed=embed)
+
+
+def memstrToNum(string):
+    string = (
+        string.replace("G", "*1073741824")
+        .replace("M", "*1048576")
+        .replace("K", "*1024")
+    )
+    num = float(eval(string))
+    return num
+
+
+def getCPUStats():
+    raw = subprocess.Popen(["mpstat", "-u", "-o", "JSON"], stdout=subprocess.PIPE)
+    data = json.loads(raw.communicate()[0])
+    user = data["sysstat"]["hosts"][0]["statistics"][0]["cpu-load"][0]["usr"]
+    system = data["sysstat"]["hosts"][0]["statistics"][0]["cpu-load"][0]["sys"]
+    return float("{:.2f}".format(user + system))
